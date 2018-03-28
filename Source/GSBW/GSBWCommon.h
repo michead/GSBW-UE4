6c0a31511a07cc2d9242e4e0f4b30265633811d9
michead <simone.riccardelli@gmail.com>
2018-03-28 23:59:42 +0200

    Fix compilation error on OSX

--- Source/GSBW/GSBWCommon.h
+++ Source/GSBW/GSBWCommon.h
@@ -2,7 +2,6 @@
 
 #include "Engine.h"
 
-#define PURE_VIRTUAL                  { check(0 && "Pure Virtual Function -- Override It") }
 #define STR_ALPHABET_LC               "abcdefghijklmnopqrstuvwxyz" 
 #define IL_ALPHABET_LC                { "a", "b", "c", "d", "e",  "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" }
 #define IL_ALPHABET_UC                { "A", "B", "C", "D", "E",  "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" }
